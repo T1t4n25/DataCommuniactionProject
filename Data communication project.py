41c6d6c014769f9f75de73ec9daee3988efbf04c
Zeyad Hemeda <shadodiss@gmail.com>
2023-12-14 19:11:06 +0200

    starting code

---
+++ Data communication project.py
@@ -0,0 +1,14 @@
+divisor = 0b1011
+
+def sender(message):
+    pass
+
+def sender_CRC(message, cipher):
+    pass
+
+def receiver_CRC(message, cipher):
+    pass
+
+def receiver(message):
+    pass
+
