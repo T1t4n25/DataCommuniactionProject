cdb3a5ad1933c5a19b06b441415120a8e1f6cfe4
Zeyad Hemeda <shadodiss@gmail.com>
2023-12-15 15:29:03 +0200

    added the sender CRC algorithm

--- Data communication project.py
+++ Data communication project.py
@@ -1,10 +1,28 @@
-divisor = 0b1011
-
+divisor = '1011'
+empty_7bits = '0000000'
 def sender(message): # Haytham Work
     pass
 
 def sender_CRC(message, cipher): # Zeyad Hemeda Work
-    pass
+    toBeSent = [message,  cipher]
+    for m in range(len(toBeSent))
+        # initializing string 
+        test_str = str(toBeSent[m].encode('utf-8'))
+    
+        print("The original string is : " + str(test_str))
+
+        bit_arr1 = [format(ord(i), '08b') for i in test_str]
+
+        bit_arr = [i + empty_7bits for i in bit_arr1]
+
+        crc_bits = [format(int(i, 2) % int(divisor, 2), '08b') for i in bit_arr]
+
+        bit_arr = [int(bit_arr1[i] + crc_bits[i], 2) for i in range(len(bit_arr1))]
+
+        toBeSent[m] = bit_arr
+        # send to reciever CRC
+        receiver_CRC(toBeSent[0], toBeSent[1])
+
 
 def receiver_CRC(message, cipher): # Zeyad Hemeda Work
     pass
