222f1d10abde12985e35c3a0a28d042be9fcbdbd
Zeyad Hemeda <shadodiss@gmail.com>
2023-12-22 17:51:30 +0200

    refactor code to new changes of the reciever() addition

--- Data communication project.py
+++ Data communication project.py
@@ -2,31 +2,6 @@ from cryptography.fernet import Fernet
 divisor = '1011'
 
 
-def sender(message): # Haytham Work
-
-    # Generate a random encryption key
-    key = Fernet.generate_key()
-
-    # Create a Fernet cipher with the generated key
-    cipher = Fernet(key)
-
-    # Convert the text to bytes
-    text_bytes = message.encode('utf-8')
-
-    # Encrypt the text
-    encrypted_text = cipher.encrypt(text_bytes)
-
-    return key, encrypted_text
-
-# Usage 
-text_to_encrypt = "Hello, this is a secret message."
-key, encrypted_message = sender(text_to_encrypt)
-print("Original Text: ", text_to_encrypt)
-print("Encrypted Text: ", encrypted_message)
-
-
-#################################################################################################################
-
 def xor(a, b):# Zeyad Hemeda
     result = []
 
@@ -63,16 +38,37 @@ def mod2div(dividend, divisor2):# zeyad Hemeda
     checkword = tmp
     return checkword
 
+# use this to convert the messege from binary to string
+def bin_to_string(to_convert):# Zeyad Hemeda
+    print(to_convert)
+    string = ''.join(chr(i) for i in to_convert)
+    return string
 
-def sender_crc(message, encrypted_text,key):  # Zeyad Hemeda Work
-    to_be_sent = [message, encrypted_text]
+def sender(message): # Haytham Work
+
+    # Generate a random encryption key
+    key = Fernet.generate_key()
+
+    # Create a Fernet cipher with the generated key
+    cipher = Fernet(key)
+    print("Original Text: ", message)
+    # Convert the text to bytes
+    text_bytes = message.encode('utf-8')
+    print("Encoded Text: ", text_bytes)
+    # Encrypt the text
+    encrypted_text = cipher.encrypt(text_bytes)
+    print("Encrypted Text: ", encrypted_text)
+
+    sender_crc(encrypted_text, cipher)
+
+#################################################################################################################
+
+def sender_crc(message, cipher):  # Zeyad Hemeda Work
+    to_be_sent = [message, cipher]
     for m in range(len(to_be_sent)):
-        # initializing string
-        test_str = str(to_be_sent[m].encode('utf-8'))
-
-        print("The original string is : " + test_str)
+        str_conv = str(to_be_sent[m])
         # convert from string to int
-        bit_arr1 = [ord(i) for i in test_str]
+        bit_arr1 = [ord(i) for i in str_conv]
         # shift the bits to get the remainder
         bit_arr = [format(i << 3, '08b') for i in bit_arr1]
         # here we calculate the crc bits
@@ -81,7 +77,7 @@ def sender_crc(message, encrypted_text,key):  # Zeyad Hemeda Work
         # the final message bits embedded with crc bits
         bit_arr = [format(bit_arr1[i], '08b') + crc_bits[i] for i in range(len(bit_arr1))]
         to_be_sent[m] = bit_arr
-        # send to receiver CRC
+        # send to receiver CRC (still needs work not finished)
         for i in range(len(to_be_sent[m])):
             x = receiver_crc(to_be_sent[m][i], m)
             if x != 1:
@@ -90,35 +86,37 @@ def sender_crc(message, encrypted_text,key):  # Zeyad Hemeda Work
 
 
 ###############################################################
+# these contain the transmitted messege as bytes
 correct_bytes = []
 correct_cipher = []
 
 
 def receiver_crc(receivedMessage, flag):  # Zeyad Hemeda Work
-    print(receivedMessage, flag)
+
     if (int(mod2div(receivedMessage, divisor), 2) == 0) and flag == 0:
-        print(1)
         correct_bytes.append(int(receivedMessage, 2) >> 3)
         return 1
+    
     elif (int(mod2div(receivedMessage, divisor), 2) == 0) and flag == 1:
-        print(2)
         correct_cipher.append(int(receivedMessage, 2) >> 3)
         return 1
+    
     elif int(mod2div(receivedMessage, divisor), 2) != 0:  # packet error not correct
         print(mod2div(receivedMessage, divisor))
         return 0
-def bin_to_string(original_message, cipher):
-    print(original_message)
-    string1 = ''.join(chr(i) for i in original_message)
-    string2 = ''.join(chr(i) for i in cipher)
-    return string1, string2
+    
+#########################################################################################
 
 
-def receiver(message, encrypted_text): # not determined
+def receiver(message, encrypted_text): # Zeyad Mohsen Work
     pass
 
-def welcome_page(): # not determined
+def welcome_page(): # Zeyad ElHarty and Saed Ragheb work
     pass
-sender_crc('ahmed', 'kh')
-message, sipher = bin_to_string(correct_bytes, correct_cipher)
-print(message, sipher)
+
+if __name__ == "__main__":
+    messege = 'mahmoud'
+    sender(messege)
+    print("messege", bin_to_string(correct_bytes))
+    print("cipher", bin_to_string(correct_cipher))
+
